2814ec556432f48c688bbc75283b01596c3dce76
Patrick Dacius <Patrickdacius27@gmail.comgit config --global credential.helper store>
2024-07-11 00:06:33 -0400

    finished

--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,7 +2,38 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+    static #allCarts = []
+    #cartItems = []
 
+    constructor() {
+        this.id = getId()
+
+        ShoppingCart.#allCarts.push(this);
+    }
+    createItem(name, price) {
+        const newItems = new CartItem(name, price)
+        this.#cartItems.push(newItems)
+        return newItems
+    }
+    getItems() {
+        return [...this.#cartItems]
+    }
+    removeItem(id) {
+
+    }
+    getTotal() {
+        return this.#cartItems.reduce((total, item) => total + item.price, 0)
+    }
+    removeItem(id) {
+        this.#cartItems = this.#cartItems.filter(item => item.id !== id);
+    }
+    static listAll() {
+        return [...this.#allCarts]
+
+    }
+    static findBy(id) {
+        return this.#allCarts.find(cart => cart.id === id)
+    }
 }
 
 export default ShoppingCart;
